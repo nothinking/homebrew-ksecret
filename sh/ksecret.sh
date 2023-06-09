df377f8213f988b6ee4a74d53de3ad31f18bc8b6
sean.go <sean.go@kakaocorp.com>
2023-06-09 16:31:30 +0900

    .

--- sh/ksecret.sh
+++ sh/ksecret.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo 'Hello World'
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: ksecret [-h | search | status | update]"
+   echo "options:"
+   echo "-h        Print this Help."
+   echo "search    시크릿이 있는 지 확인."
+   echo "status    시크릿의 유효기간 확인."
+   echo "update    시크릿의 갱신."
+   echo
+}
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
+
+METHOD=ksecret-$1
+shift
+$METHOD "$@"
+
